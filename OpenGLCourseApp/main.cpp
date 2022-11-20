8d6fb0f6ccd5340bf648dc88af8b03fed491a52c
koboko34 <66181314+koboko34@users.noreply.github.com>
2022-11-20 22:12:46 +0000

    Added glm
    - added opengl maths library
    - moving triangle left and right using unform variables (temporary)

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader, uniformXMove;
+
+bool direction = true;
+float triOffset = 0.f;
+float triMaxOffset = 0.5f;
+float triInc = 0.0005f;
 
 static const char* vShader = "									\n\
 #version 330													\n\
 																\n\
 layout (location = 0) in vec3 pos;								\n\
 																\n\
+uniform float xMove;											\n\
+																\n\
 void main()														\n\
 {																\n\
-	gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);	\n\
+	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);	\n\
 }";
 
 static const char* fShader = "									\n\
@@ -90,6 +98,7 @@ void CompileShaders()
 		return;
 	}
 	
+	uniformXMove = glGetUniformLocation(shader, "xMove");
 
 }
 
@@ -170,11 +179,28 @@ int main()
 		// Get and handle user input events
 		glfwPollEvents();
 
+		if (direction)
+		{
+			triOffset += triInc;
+		}
+		else
+		{
+			triOffset -= triInc;
+		}
+
+		if (abs(triOffset) >= triMaxOffset)
+		{
+			direction = !direction;
+		}
+
 		// Clear window
 		glClearColor(0.f, 0.f, 0.f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
+
+		glUniform1f(uniformXMove, triOffset);
+
 		glBindVertexArray(VAO);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
